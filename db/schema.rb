b55e8f8ed2bdf66108f39912ea5c4b149d5f9828
Keshden <bleonrey7@gmail.com>
2017-01-13 12:19:06 -0700

    create the many-to-many assoc between categories and articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111052435) do
+ActiveRecord::Schema.define(version: 20170113183633) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
